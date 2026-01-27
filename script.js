f6b81779e1406aaaae382bd568285e1d47b3b536
renzhao <3251qq@163.com>
2026-01-27 13:59:56 +0800

    add script

---
+++ script.js
@@ -0,0 +1,23 @@
+// Define main function (script entry)
+
+const prependRule = [
+  // nas和pt
+  "DOMAIN-SUFFIX,renzhao.cc,DIRECT",
+  "DOMAIN-KEYWORD,kufei,DIRECT",
+  "DOMAIN-KEYWORD,rousi,DIRECT",
+  "DOMAIN-SUFFIX,crabpt.vip,DIRECT",
+  "DOMAIN-KEYWORD,pttime,DIRECT",
+  "DOMAIN-KEYWORD,m-team,DIRECT",
+  "DOMAIN-KEYWORD,xingyungept,DIRECT",
+  "DOMAIN-KEYWORD,ptskit,DIRECT",
+  // 代理
+  "DOMAIN-SUFFIX,udp-test.com,可可云",
+  "DOMAIN-KEYWORD,claude,可可云",
+  "DOMAIN-SUFFIX,openxlab.org.cn,可可云"
+];
+
+function main(config, profileName) {
+  let oldrules = config["rules"];
+  config["rules"] = prependRule.concat(oldrules);
+  return config;
+};
